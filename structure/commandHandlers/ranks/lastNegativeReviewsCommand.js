a14536ae80d3b9033cf85dfc168b907b6c64ac8f
WhitesLove <kubai-rita5@gmail.com>
2025-02-17 17:56:09 +0300

    Version 0.1 Base Fixes

--- structure/commandHandlers/ranks/lastNegativeReviewsCommand.js
+++ structure/commandHandlers/ranks/lastNegativeReviewsCommand.js
@@ -5,10 +5,5 @@ export default async function handleLastNegativeReviewsCommand(interaction, pool
     const member = interaction.options.getUser('member');
     if (!member) return interaction.reply({ content: 'Выберите участника.', flags: MessageFlags.Ephemeral });
 
-    const reviews = await pool.query(
-        'SELECT review_text, "timestamp" FROM reviews WHERE target_user = $1 AND is_positive = false ORDER BY timestamp DESC',
-        [member.id]
-    );
-
-    await sendPaginatedReviews(interaction, reviews.rows, member, 1);
+    await sendPaginatedReviews(interaction, pool, member, 1, false);
 }
--- structure/commandHandlers/ranks/lastReviewsCommand.js
+++ structure/commandHandlers/ranks/lastReviewsCommand.js
@@ -5,10 +5,5 @@ export default async function handleLastReviewsCommand(interaction, pool) {
     const member = interaction.options.getUser('member');
     if (!member) return interaction.reply({ content: 'Выберите участника.', flags: MessageFlags.Ephemeral });
 
-    const reviews = await pool.query(
-        'SELECT review_text, is_positive, "timestamp" FROM reviews WHERE target_user = $1 ORDER BY timestamp DESC',
-        [member.id]
-    );
-
-    await sendPaginatedReviews(interaction, reviews.rows, member);
+    await sendPaginatedReviews(interaction, pool, member, 1);
 }
