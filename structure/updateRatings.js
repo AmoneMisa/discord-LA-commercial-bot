f47eaed7e14278d68b90d758bb7becf3ad995d15
WhitesLove <kubai-rita5@gmail.com>
2025-02-21 17:10:46 +0300

    Version 0.2;
    fixes

--- structure/updateRatings.js
+++ structure/updateRatings.js
@@ -5,14 +5,21 @@ export default async function updateRatings(pool) {
         UPDATE users
         SET rating = (
             SELECT COALESCE(
-                           (SUM(CASE WHEN is_positive THEN 1 ELSE 0 END)::FLOAT / NULLIF(COUNT(*), 1) * 100)
-                               * (1 - EXP(- NULLIF(COUNT(*), 1) / $1)),
+                           (
+                               SUM(
+                                       CASE
+                                           WHEN is_positive THEN 1
+                                           ELSE 0
+                                           END
+                               )::FLOAT
+                                   / COALESCE(COUNT(*), 1)::FLOAT * 100
+                               ) * (1 - EXP(- COALESCE(COUNT(*), 1)::FLOAT / $1)),
                            0
                    )
             FROM reviews
             WHERE target_user = users.user_id
               AND timestamp >= NOW() - INTERVAL '30 days'
-            );
+        );
     `, [weight]);
 
     console.log('✅ Рейтинги обновлены с учётом отзывов за 30 дней!');
