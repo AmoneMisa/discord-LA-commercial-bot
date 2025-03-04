a8dbffff99d6723fcbdd7e4439f861f4c6dabc5f
WhitesLove <kubai.rita5@gmail.com>
2025-03-04 03:24:22 +0300

    Version 0.4 - Added bets system

--- structure/interactions/modals.js
+++ structure/interactions/modals.js
@@ -1,8 +1,4 @@
 import sendReview from "../commandHandlers/ranks/sendReview.js";
-import handleCreateRaidModal from "../commandHandlers/subscribe/handleCreateRaidModal.js";
-import notifySellerMessageSend from "../commandHandlers/subscribe/notifySellerMessageSend.js";
-import handleSellerAnswerToBuyerModal from "../commandHandlers/subscribe/handleSellerAnswerToBuyerModal.js";
-import notifySellerResponse from "../commandHandlers/responses/notifySellerResponse.js";
 import betContinueHandler from "../commandHandlers/bets/betContinueHandler.js";
 
 /**
@@ -28,22 +24,6 @@ import betContinueHandler from "../commandHandlers/bets/betContinueHandler.js";
 export default async function (interaction, pool, client) {
     if (interaction.fields.fields.get('review_text') && interaction.fields.getTextInputValue('review_text')) {
         await sendReview(interaction, pool, client);
-    }
-
-    if (interaction.fields.fields.get('buyer_nickname') && interaction.fields.getTextInputValue('buyer_nickname')) {
-        await notifySellerMessageSend(interaction, pool, client);
-    }
-
-    if (interaction.fields.fields.get('raid_buyer_nickname') && interaction.fields.getTextInputValue('raid_buyer_nickname')) {
-        await notifySellerResponse(interaction, pool, client);
-    }
-
-    if (interaction.customId === 'create_raid_modal') {
-        await handleCreateRaidModal(interaction, pool);
-    }
-
-    if (interaction.customId.startsWith('raid_buy_answer_')) {
-        await handleSellerAnswerToBuyerModal(interaction, pool, client);
     }
 
     if (interaction.customId.startsWith('bet_modal')) {
