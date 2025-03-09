3902804a669a9b1dc39557dd6f2b55ab59bb47c9
WhitesLove <kubai.rita5@gmail.com>
2025-03-10 02:11:30 +0300

    Version 0.5.3 Added main translations

--- structure/commands/userSettingsCommands.js
+++ structure/commands/userSettingsCommands.js
@@ -10,6 +10,7 @@ export default [
             .setRequired(true)
             .addChoices({name: "Русский", value: "ru"},
                 {name: "Українська", value: "ua"},
+                {name: "Беларуская", value: "by"},
                 {name: "English", value: "en"},
     {name: "中文", value: "ch"}))
 ]
