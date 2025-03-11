ef885748bd7ed6bd8f11db64e5182472b2ed207c
WhitesLove <kubai.rita5@gmail.com>
2025-03-11 13:13:49 +0300

    Version 0.5.4 Fix odds

--- structure/commandHandlers/bets/updateBetModal.js
+++ structure/commandHandlers/bets/updateBetModal.js
@@ -1,9 +1,30 @@
 import {ActionRowBuilder, ButtonStyle, MessageFlags, ModalBuilder, TextInputBuilder, TextInputStyle} from "discord.js";
 import i18n from "../../../locales/i18n.js";
 import {getUserLanguage} from "../../dbUtils.js";
+import {getActiveEvent} from "../../utils.js";
 
 export default async function (interaction, pool) {
     const lang = await getUserLanguage(interaction.user.id, pool);
+
+    const event = await getActiveEvent(pool);
+    if (!event) {
+        return await interaction.reply({
+            content: i18n.t("errors.noBetEventExist", { lng: lang}),
+            flags: MessageFlags.Ephemeral
+        });
+    }
+    const bet = await pool.query(`SELECT *
+                                  FROM bets
+                                  WHERE event_id = $1
+                                    AND user_id = $2`, [event.id, interaction.user.id]);
+    if (!bet.rows.length) {
+        await interaction.reply({
+            content: i18n.t("errors.betDontExist", { lng: lang}),
+            flags: MessageFlags.Ephemeral
+        });
+        return;
+    }
+
     const modal = new ModalBuilder()
         .setCustomId(`bet_update_modal`)
         .setTitle(i18n.t("buttons.updateBetTitle", { lng: lang}));
