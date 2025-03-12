0534225221494e6ac2c7ca33ebcfaff6acb1a5c6
WhitesLove <kubai.rita5@gmail.com>
2025-03-12 18:34:03 +0300

    Version 0.5.6 Fix handle info command; Fix show review modal;

--- structure/commandHandlers/ranks/showReviewModal.js
+++ structure/commandHandlers/ranks/showReviewModal.js
@@ -6,20 +6,22 @@ import {getUserLanguage} from "../../dbUtils.js";
  * Displays a modal for the user to leave a review based on the specified action.
  *
  * @param {object} interaction - The interaction object representing the user interaction with the bot.
+ * @param pool
  * @param {string} action - The type of review action, either 'upvote' or 'downvote'.
  * @param {string} userId - The ID of the user for whom the review is being left.
  * @return {Promise<void>} A promise that resolves once the review modal has been displayed to the user.
  */
-export default async function showReviewModal(interaction, action, userId) {
+export default async function showReviewModal(interaction, pool, action, userId) {
+    const lang = await getUserLanguage(interaction.user.id, pool);
     const modal = new ModalBuilder()
         .setCustomId(`review_${action}_${userId}`)
-        .setTitle(action === 'upvote' ? i18n.t("buttons.leavePositiveReview", {lng: await getUserLanguage(interaction.user.id, pool)}) : i18n.t("buttons.leaveNegativeReview", {lng: await getUserLanguage(interaction.user.id, pool)}));
+        .setTitle(action === 'upvote' ? i18n.t("buttons.leavePositiveReview", {lng: lang}) : i18n.t("buttons.leaveNegativeReview", {lng: lang}));
 
     const input = new TextInputBuilder()
         .setCustomId('review_text')
-        .setLabel(i18n.t("buttons.fillReviewTitle", {lng: await getUserLanguage(interaction.user.id, pool)}))
+        .setLabel(i18n.t("buttons.fillReviewTitle", {lng: lang}))
         .setStyle(TextInputStyle.Paragraph)
-        .setPlaceholder(i18n.t("buttons.fillReviewText", {lng: await getUserLanguage(interaction.user.id, pool)}))
+        .setPlaceholder(i18n.t("buttons.fillReviewText", {lng: lang}))
         .setMinLength(10)
         .setMaxLength(300)
         .setRequired(true);
